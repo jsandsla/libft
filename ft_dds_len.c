c65f57d2e479ce1cfb1846c19d5415317a760a57
jsandsla <jsandsla@student.21-school.ru>
2020-11-11 00:49:36 +0500

    dds_len_bug

--- ft_dds_len.c
+++ ft_dds_len.c
@@ -6,7 +6,7 @@
 /*   By: jsandsla <jsandsla@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 19:20:33 by jsandsla          #+#    #+#             */
-/*   Updated: 2020/11/09 01:16:39 by jsandsla         ###   ########.fr       */
+/*   Updated: 2020/11/11 00:49:15 by jsandsla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 size_t			ft_dds_len(t_dds *dds)
 {
 	size_t	len;
-	size_t	l;
 	size_t	i;
 
 	len = 0;
 	while (i < dds->dd.len)
 	{
-		l = dds->dd.ptr[i].m->len;
-		len += l > 0 ? l - 1 : 0;
+		len += dds->dd.ptr[i].m->len;
 		i += 1;
 	}
 	return (len);
